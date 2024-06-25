f204586b19defb025bab99a7611ab965cefe8f41
Felipe Hubner <feliperodrigueshubner@hotmail.com>
2024-06-25 17:38:32 -0300

    hotfix: erro no webhook desconectarInstancia

--- api/webhook/desconectarInstancia.php
+++ api/webhook/desconectarInstancia.php
@@ -7,11 +7,12 @@ require_once(__DIR__ . '/../utils/functions.php');
 require_once(__DIR__ . '/webhook-config.php');
 
 $req = $json;
+$affected = 0;
 
-$zApiIdInstancia = mysqli_real_escape_string($db,$req['instanceId']);
+$currentZApiIdInstancia = mysqli_real_escape_string($db,$req['instanceId']);
 
 validate([
-    $zApiIdInstancia
+    $currentZApiIdInstancia
 ]);
 
 $sql = "SELECT 
@@ -23,25 +24,27 @@ $sql = "SELECT
 
     FROM cGroups cg 
     INNER JOIN instances i USING(idInstance)
-    WHERE i.zApiIdInstancia = '{$zApiIdInstancia}';
+    WHERE i.zApiIdInstancia = '{$currentZApiIdInstancia}';
 ";
 
-if(!$result = mysqli_query($db,$sql)) error('Falha ao buscar grupos comerciais');
-if(mysqli_num_rows($result) < 1) error('Nenhum registro para essa instância');
-while($row = mysqli_fetch_assoc($result)){
+if(!$resultGC = mysqli_query($db,$sql)) error('Falha ao buscar grupos comerciais');
+
+if(mysqli_num_rows($resultGC) < 1) error('Nenhum registro para essa instância');
+
+while($row = mysqli_fetch_assoc($resultGC)){
     $idCGroup = $row['idCGroup'];
+    $oldZApiTokenInstancia = $row['zApiTokenInstancia'];
+    $oldZApiSecret         = $row['zApiSecret'];
 
-    $zApiTokenInstancia = $row['zApiTokenInstancia'];
-    $zApiSecret         = $row['zApiSecret'];
-
-    atualizarWebhookZApiReceber($zApiIdInstancia,$zApiTokenInstancia,$zApiSecret,true);
-    atualizarWebhookZApiDesconectar($zApiIdInstancia,$zApiTokenInstancia,$zApiSecret,true);
+    //atualizarWebhookZApiReceber($currentZApiIdInstancia,$oldZApiTokenInstancia,$oldZApiSecret,true);
+    //atualizarWebhookZApiDesconectar($currentZApiIdInstancia,$oldZApiTokenInstancia,$oldZApiSecret,true);
 
     // buscar a próxima instancia
     $sql = "SELECT * FROM instances WHERE idCGroup = '{$idCGroup}' ORDER BY orderNumber ASC;";
-    if(!$result = mysqli_query($db,$sql)) error('Falha ao buscar instancias');
+    if(!$resultInstance = mysqli_query($db,$sql)) error('Falha ao buscar instancias');
+    if(mysqli_num_rows($resultInstance) < 1) continue;
 
-    while($row = mysqli_fetch_assoc($result)){
+    while($row = mysqli_fetch_assoc($resultInstance)){
         $zApiIdInstancia    = $row['zApiIdInstancia'];
         $zApiTokenInstancia = $row['zApiTokenInstancia'];
         $zApiSecret         = $row['zApiSecret'];
@@ -52,11 +55,14 @@ while($row = mysqli_fetch_assoc($result)){
         if($status['ok'] != true) continue;
 
         $sql = "UPDATE cGroups SET idInstance = '{$idInstance}' WHERE idCGroup = '{$idCGroup}';";
-        if(!$result = mysqli_query($db,$sql)) error('Falha ao definir instancia no Grupo Comercial');
 
+        if(!$resultUpdate = mysqli_query($db,$sql)) error('Falha ao definir instancia no Grupo Comercial');
+        $affected += mysqli_affected_rows($db);
         atualizarWebhookZApiReceber($zApiIdInstancia,$zApiTokenInstancia,$zApiSecret);
         atualizarWebhookZApiDesconectar($zApiIdInstancia,$zApiTokenInstancia,$zApiSecret);
         break;
     }
 
 }
+
+success("Concluído ({$affected})");
